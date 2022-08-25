623e22ffaacb75b44e302509e409cde3a858b33a
Mikhail Malafeew <malafeew2016@gmail.com>
2022-08-25 16:23:19 +0300

    Ex_001

---
+++ EX-002/Program.cs
@@ -0,0 +1,28 @@
+﻿void FillArray(int[]MyArray)
+{
+    for(int i=0; i < MyArray.Length; i++)
+    {
+        MyArray[i]=new Random().Next(-100,101);
+    }
+}
+void PrintArr(int[] MyArray)
+{
+    Console.WriteLine("[{0}]", string.Join(", ", MyArray));
+}
+int SumOddNumbers(int[] MyArray)
+{
+    int sum =0;
+    for(int i = 1; i< MyArray.Length; i++)
+    {
+        sum+=MyArray[i++];
+
+    }
+    return sum;
+}
+//GETTINGCODE//
+Console.Write("Введите размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] MyArray = new int[size];
+FillArray(MyArray);
+PrintArr(MyArray);
+Console.WriteLine($"Сумма чисел, стоящих на нечетных позициях равно {SumOddNumbers(MyArray)} ");
